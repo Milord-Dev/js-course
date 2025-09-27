79e7d92311e1076b5f2979067eafa7ab0d7b3156
ricardo milord <milord.dev05@gmail.com>
2025-09-27 15:31:23 -0500

    feat: add initial BMI calculator files
    Add HTML and JavaScript files for BMI calculator functionality

---
+++ entregables/02-pruebaRefuerzo/appPrueba.js
@@ -0,0 +1,9 @@
+console.log('-- INDICE DE MASA CORPORAL --');
+console.log('----------------------------');
+
+let estatura = 165; //cm
+let peso = 60; //kg
+
+for(let indiceMasaCorporal = 0; indiceMasaCorporal <= 7; indiceMasaCorporal++ ){
+    
+}
