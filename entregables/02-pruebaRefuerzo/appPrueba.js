fa7bf28ed242024f8ca007a0405e00114e221d4f
ricardo milord <milord.dev05@gmail.com>
2025-09-27 16:16:33 -0500

    refactor: remove unused variable and add loop counter logging

--- entregables/02-pruebaRefuerzo/appPrueba.js
+++ entregables/02-pruebaRefuerzo/appPrueba.js
@@ -1,10 +1,9 @@
 console.log('-- INDICE DE MASA CORPORAL --');
 console.log('----------------------------');
 
-let indiceMasaCorporal = 0;
 let estatura = 165; //cm
 let peso = 60; //kg
 
 for(let i = 0; i <= 7; i++){
-    
+    console.log(i);
 }
