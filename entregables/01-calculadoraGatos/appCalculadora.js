8ae98c6341061b9f7c23aca124b26facfec3b451
ricardo milord <milord.dev05@gmail.com>
2025-09-27 13:26:42 -0500

    fix: correct cat age calculation for first two years
    Update the calculation logic to properly convert cat years to human years for the first two years (15 and 24 human years respectively) before continuing with the standard 4-year increment

--- entregables/01-calculadoraGatos/appCalculadora.js
+++ entregables/01-calculadoraGatos/appCalculadora.js
@@ -1,6 +1,12 @@
 console.log('-- Calculadora años Gatunos --');
 let edadHumana = 0;
-for( let edadGato = 0; edadGato <= 22; edadGato++){
-    edadHumana +=  4;
+for( let edadGato = 1; edadGato <= 22; edadGato++){
+    if(edadGato == 1){
+        edadHumana = 15
+    }else if(edadGato == 2){
+        edadHumana = 24
+    }else{
+        edadHumana +=  4;
+    }
     console.log(`El gato tiene ${edadHumana} años en edad humana`);
 }
