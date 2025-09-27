d733100f439e1a645a3461d32c8fbd9f4e4d82bb
ricardo milord <milord.dev05@gmail.com>
2025-09-27 13:21:03 -0500

    refactor(calculadoraGatos): rewrite cat age calculator logic
    Replace static age display with loop to calculate human equivalent ages

--- entregables/01-calculadoraGatos/appCalculadora.js
+++ entregables/01-calculadoraGatos/appCalculadora.js
@@ -1,4 +1,6 @@
 console.log('-- Calculadora años Gatunos --');
-let edad = 10;
-let nombreGato = "Hector";
-console.log('Edad en años humanos:', edad, " y su nombre es: ",nombreGato);
+let edadHumana = 0;
+for( let edadGato = 0; edadGato <= 22; edadGato++){
+    edadHumana +=  4;
+    console.log(`El gato tiene ${edadHumana} años en edad humana`);
+}
