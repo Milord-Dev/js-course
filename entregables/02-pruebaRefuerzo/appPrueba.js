0d4070a79caefa442921ab18417a338a089b7ce2
ricardo milord <milord.dev05@gmail.com>
2025-09-28 15:48:41 -0500

    refactor: replace hardcoded values with BMI calculation loop
    Replace static height and weight variables with nested loops to calculate BMI for a range of values

--- entregables/02-pruebaRefuerzo/appPrueba.js
+++ entregables/02-pruebaRefuerzo/appPrueba.js
@@ -1,12 +1,9 @@
 console.log('-- INDICE DE MASA CORPORAL --');
 console.log('----------------------------');
 
-let estatura = 165; //cm
-let peso = 60; //kg
-
 for(let estatura = 165; estatura <= 195; estatura += 5){
     for(let peso = 60; peso <= 80; peso ++){
-        
+
     }
     console.log(estatura);
 }
