3b2c98786624246dc2ec05a46f131b9a9cd4fdd9
ricardo milord <milord.dev05@gmail.com>
2025-09-29 10:15:53 -0500

    refactor: eliminar código redundante en el cálculo del IMC

--- entregables/02-pruebaRefuerzo/appPrueba.js
+++ entregables/02-pruebaRefuerzo/appPrueba.js
@@ -1,47 +1,6 @@
 console.log('-- INDICE DE MASA CORPORAL --');
 console.log('----------------------------');
 
-for(let estatura = 165; estatura <= 195; estatura += 5){
-    if (estatura == 165){
-        console.log(`Estatura y peso de: 165cm`);
-        console.log("----------------------------");
-        for(let peso = 60; peso <= 80; peso += 5){
-            console.log(`Estatura: ${estatura}cm, Peso: ${peso}kg`);
-        }
-    }
-    else if (estatura == 170){
-        console.log(`Estatura y peso de: 170cm`);
-        console.log("----------------------------");
-        for(let peso = 65; peso <= 85; peso += 5){
-            console.log(`Estatura: ${estatura}cm, Peso: ${peso}kg`);
-        }
-    }
-    else if (estatura == 175){
-        console.log(`Estatura y peso de: 175cm`);
-        console.log("----------------------------");
-        for(let peso = 70; peso <= 90; peso += 5){
-            console.log(`Estatura: ${estatura}cm, Peso: ${peso}kg`);
-        }
-    }
-    else if (estatura == 180){
-        console.log(`Estatura y peso de: 180cm`);
-        console.log("----------------------------");
-        for(let peso = 75; peso <= 95; peso += 5){
-            console.log(`Estatura: ${estatura}cm, Peso: ${peso}kg`);
-        }
-    }
-    else if (estatura == 185){
-        console.log(`Estatura y peso de: 185cm`);
-        console.log("----------------------------");
-        for(let peso = 80; peso <= 100; peso += 5){
-            console.log(`Estatura: ${estatura}cm, Peso: ${peso}kg`);
-        }
-    }
-    else if (estatura == 190){
-        console.log(`Estatura y peso de: 190cm`);
-        console.log("----------------------------");
-        for(let peso = 85; peso <= 105; peso += 5){
-            console.log(`Estatura: ${estatura}cm, Peso: ${peso}kg`);
-        }
-    }
+for(let i = 0; i <= 3; i =+ 5){
+
 }
