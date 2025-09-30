e2f9116eee595135bfbdf02dab4da87f7962ca2d
ricardo milord <milord.dev05@gmail.com>
2025-09-30 13:46:59 -0500

    fix: ajustar los límites de peso en función de la altura en el cálculo del IMC

--- entregables/02-pruebaRefuerzo/appPrueba.js
+++ entregables/02-pruebaRefuerzo/appPrueba.js
@@ -2,8 +2,8 @@ console.log('-- INDICE DE MASA CORPORAL --');
 console.log('----------------------------');
 
 for(let altura = 165; altura <= 195; altura = altura + 5){
-    let pesomin = 60;
-    let pesomax = 80;
+    let pesomin = 60 + (altura - 165);
+    let pesomax = 80 + (altura - 165);
     
     for(let peso = pesomin; peso <= pesomax; peso = peso + 10){
         console.log(`Altura: ${altura} cm - Peso: ${peso}`);
