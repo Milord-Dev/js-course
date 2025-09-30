b7f5012c2b9f897f9ffaa7591faa23c06ecb4bdd
ricardo milord <milord.dev05@gmail.com>
2025-09-30 10:34:00 -0500

    fix: corregir el rango y el incremento en el bucle de impresión del IMC

--- entregables/02-pruebaRefuerzo/appPrueba.js
+++ entregables/02-pruebaRefuerzo/appPrueba.js
@@ -1,6 +1,6 @@
 console.log('-- INDICE DE MASA CORPORAL --');
 console.log('----------------------------');
 
-for(let i = 0; i <= 3; i =+ 5){
-
+for(let i = 1; i <= 50; i =+ 5){
+    console.log(i);
 }
