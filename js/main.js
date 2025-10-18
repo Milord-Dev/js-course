80edace463f6c6f157ad17e182972ddb9a6d3ec6
ricardo milord <milord.dev05@gmail.com>
2025-10-18 14:13:34 -0500

    fix: corregir la estructura del código y mejorar la legibilidad en main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 /*
     JS para comprobacion de formulario
-*/ 
 
 // Capturar el valor del input
 const nickInput = document.getElementById('nick');
@@ -11,3 +10,11 @@ console.log(nickInput.value);
 const tamanoItem = document.getElementById('tamano');
 console.log("Saca el valor de la poscion selecionada: " + tamanoItem.value);
 console.log("Saca el texto de la seleccion del usuario: " + tamanoItem.options[tamanoItem.selectedIndex].text);
+*/ 
+
+// Variables
+let nickUsuario = "";
+let correoElectronico = "";
+let tamanoJuego = "";
+
+// Comprobacion de formulario
