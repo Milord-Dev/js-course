b24a938599bcced0a97f6188b8d81fc6b507c63e
ricardo milord <milord.dev05@gmail.com>
2025-10-18 14:00:01 -0500

    fix: corregir el ID del input para el nombre y agregar captura del tamaño del juego

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@
 */ 
 
 // Capturar el valor del input
-const nickInput = document.getElementById('nicka');
+const nickInput = document.getElementById('nick');
 console.log(nickInput.nodeType);
+nickInput.value = "Milord"
 console.log(nickInput.value);
+
+const tamanoItem = document.getElementById('tamano');
+console.log("Saca el valor de la poscion selecionada: " + tamanoItem.value);
+console.log("Saca el texto de la seleccion del usuario: " + tamanoItem.options[tamanoItem.selectedIndex].text);
