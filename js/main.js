d2c4a85baf55062904d910a9e9f17462b4073a31
ricardo milord <milord.dev05@gmail.com>
2025-10-27 00:48:56 -0500

    fix: agregar mensajes de error en el formulario y mejorar la validación de campos

--- js/main.js
+++ js/main.js
@@ -8,27 +8,28 @@ const correoInput = document.getElementById('email');
 const edadInput = document.getElementById('edad');
 const tamanoInput = document.getElementById('tamano');
 const formulario = document.getElementById('formularioEntrada');
+const error = document.getElementById('error'); 
 
 //Funciones
 function comprobacionForm(event){
     if (nickInput.value.length < 4 || correoInput.value.length == 0){
-        alert('Por favor, rellena todos los campos');
         console.log('Faltan campos por rellenar');
         nickInput.focus();
         event.preventDefault();
+        error.innerText = 'Por favor, rellena todos los campos obligatorios';
         return false;
     }
     else if (edadInput.value < 7 || edadInput.value > 99){
-        alert('Por favor, introduce una edad válida (entre 7 y 99 años)');
         console.log('edad no valida');
         edadInput.focus();
+        error.innerText = 'Por favor, introduce una edad válida (entre 7 y 99 años)';
         event.preventDefault();
         return false;
     }
     else if (tamanoInput.value == "0"){
-        alert('Por favor, selecciona un tamaño del juego');
         console.log('No se ha seleccionado un tamaño del juego');
         tamanoInput.focus();
+        error.innerText = 'Por favor, selecciona un tamaño del juego';
         event.preventDefault();
         return false;
     }
