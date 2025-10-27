d36bcbbeefdfc19eab2a0b752e1c08be7382db0d
ricardo milord <milord.dev05@gmail.com>
2025-10-26 23:51:58 -0500

    Se agrego la opcion de elegir plataformas en el index. tambien se agrego el enlace para regresar al formulario en el juego.html y se agrego a la funcion una condicional que comprueba si elegio la plataforma

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@
 //variables
 const nickInput = document.getElementById('nick');
 const correoInput = document.getElementById('email');
-const tamanoInput = document.getElementById('tamano');
 const edadInput = document.getElementById('edad');
+const plataformaInput = document.getElementById('plataforma');
+const tamanoInput = document.getElementById('tamano');
 const formulario = document.getElementById('formularioEntrada');
 
 //Funciones
@@ -18,17 +19,24 @@ function comprobacionForm(event){
         event.preventDefault();
         return false;
     }
-    else if (tamanoInput.value == "0"){
-        alert('Por favor, selecciona un tamaño del juego');
-        console.log('No se ha seleccionado un tamaño del juego');
-        tamanoInput.focus();
-        event.preventDefault();
-        return false;
-    }
     else if (edadInput.value < 7 || edadInput.value > 99){
         alert('Por favor, introduce una edad válida (entre 7 y 99 años)');
         console.log('edad no valida');
         edadInput.focus();
+        event.preventDefault();
+        return false;
+    }
+    else if (plataformaInput.value == "0"){
+        alert('Por favor, selecciona una plataforma');
+        console.log('No se ha seleccionado una plataforma');
+        plataformaInput.focus();
+        event.preventDefault();
+        return false;
+    }
+    else if (tamanoInput.value == "0"){
+        alert('Por favor, selecciona un tamaño del juego');
+        console.log('No se ha seleccionado un tamaño del juego');
+        tamanoInput.focus();
         event.preventDefault();
         return false;
     }
