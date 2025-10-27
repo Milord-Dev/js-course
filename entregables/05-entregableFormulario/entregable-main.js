b7d5b321fcf4c234b95f4b6871da4249c3e96390
ricardo milord <milord.dev05@gmail.com>
2025-10-27 00:15:53 -0500

    feat: agregar archivos HTML, CSS y JS para el formulario de entrada del juego

---
+++ entregables/05-entregableFormulario/entregable-main.js
@@ -0,0 +1,50 @@
+/**
+ *  Iniciaremos con el verdadero desarrollo de nuestra aplicación
+ */
+
+//variables
+const nickInput = document.getElementById('nick');
+const correoInput = document.getElementById('email');
+const edadInput = document.getElementById('edad');
+const plataformaInput = document.getElementById('plataforma');
+const tamanoInput = document.getElementById('tamano');
+const formulario = document.getElementById('formularioEntrada');
+
+//Funciones
+function comprobacionForm(event){
+    if (nickInput.value.length < 4 || correoInput.value.length == 0){
+        alert('Por favor, rellena todos los campos');
+        console.log('Faltan campos por rellenar');
+        nickInput.focus();
+        event.preventDefault();
+        return false;
+    }
+    else if (edadInput.value < 7 || edadInput.value > 99){
+        alert('Por favor, introduce una edad válida (entre 7 y 99 años)');
+        console.log('edad no valida');
+        edadInput.focus();
+        event.preventDefault();
+        return false;
+    }
+    else if (plataformaInput.value == "0"){
+        alert('Por favor, selecciona una plataforma');
+        console.log('No se ha seleccionado una plataforma');
+        plataformaInput.focus();
+        event.preventDefault();
+        return false;
+    }
+    else if (tamanoInput.value == "0"){
+        alert('Por favor, selecciona un tamaño del juego');
+        console.log('No se ha seleccionado un tamaño del juego');
+        tamanoInput.focus();
+        event.preventDefault();
+        return false;
+    }
+    else{
+        alert('Formulario enviado correctamente');
+    }
+    return true;
+}
+
+//Evento
+formulario.addEventListener('submit', comprobacionForm)
