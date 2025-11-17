8c0fa89fa4605d7d9bbd280f7af8703850a740ca
ricardo milord <milord.dev05@gmail.com>
2025-11-17 01:13:25 -0500

    feat: agregar archivos HTML, CSS y JS para la validación del formulario

---
+++ entregables/06-entregableValidacion/entregable_main.js
@@ -0,0 +1,55 @@
+/**
+ *  Iniciaremos con el verdadero desarrollo de nuestra aplicación
+ */
+
+//variables
+const nickInput = document.getElementById('nick');
+const correoInput = document.getElementById('email');
+const edadInput = document.getElementById('edad');
+const tamanoInput = document.getElementById('tamano');
+const formulario = document.getElementById('formularioEntrada');
+const error = document.getElementById('error'); 
+
+//comprobacion de null 
+if (!nickInput || !correoInput || !edadInput || !tamanoInput || !formulario || !error){
+    console.error('No se han podido obtener todos los elementos del DOM necesarios');
+}
+
+//Funciones
+function comprobacionForm(event){
+    if (nickInput.value.length < 4 && nickInput.value.length > 15){
+        console.log('Faltan campos por rellenar o nick no valido');
+        nickInput.focus();
+        event.preventDefault();
+        error.innerText = 'Por favor, rellena todos los campos obligatorios';
+        return false;
+    }
+    else if (correoInput.value.length == 0){
+        console.log('Faltan campos por rellenar');
+        correoInput.focus();
+        event.preventDefault();
+        error.innerText = 'Por favor, rellena todos los campos obligatorios';
+        return false;
+    }
+    else if (edadInput.value < 7 || edadInput.value > 80){
+        console.log('edad no valida');
+        edadInput.focus();
+        error.innerText = 'Por favor, introduce una edad válida (entre 7 y 80 años)';
+        event.preventDefault();
+        return false;
+    }
+    else if (tamanoInput.value == "0"){
+        console.log('No se ha seleccionado un tamaño del juego');
+        tamanoInput.focus();
+        error.innerText = 'Por favor, selecciona un tamaño del juego';
+        event.preventDefault();
+        return false;
+    }
+    else{
+        alert('Formulario enviado correctamente');
+    }
+    return true;
+}
+
+//Evento
+formulario.addEventListener('submit', comprobacionForm);
