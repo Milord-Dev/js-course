54a2dea9685f2d83e12fcb4c76a913ade440943e
ricardo milord <milord.dev05@gmail.com>
2025-11-20 14:49:29 -0500

    feat: corregir ID del formulario y agregar archivo JS para validación de campos

---
+++ entregables/07-entregableConversor/js/formMain.js
@@ -0,0 +1,8 @@
+//Constantes
+const formulario = document.getElementById('formularioUsuario');
+const nickInput = document.getElementById('nick');
+const emailInput = document.getElementById('emaik');
+const passwordInput = document.getElementById('password');
+const btnEnviar = document.getElementById('btn_enviar');
+
+//Funciones de validacion de campos
