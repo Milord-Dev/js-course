f919bf3970f5928ae149c4e3bd83f783fc42628d
ricardo milord <milord.dev05@gmail.com>
2025-11-21 13:15:31 -0500

    feat: agregar función de validación para el campo de nick en el formulario de usuario

--- entregables/07-entregableConversor/js/formMain.js
+++ entregables/07-entregableConversor/js/formMain.js
@@ -6,3 +6,11 @@ const passwordInput = document.getElementById('password');
 const btnEnviar = document.getElementById('btn_enviar');
 
 //Funciones de validacion de campos
+function validacionFormulario(event){
+    if (nickInput.value.length < 4 || nickInput.value.length > 15){
+        error.innerText = 'El nick debe tener entre 4 y 15 caracteres';
+        nickInput.focus();
+        event.preventDefault();
+        return false;
+    }
+}
