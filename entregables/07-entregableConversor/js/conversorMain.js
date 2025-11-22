b5905b274a2142584af1b9b0e711ba9fc099532d
ricardo milord <milord.dev05@gmail.com>
2025-11-21 22:08:15 -0500

    feat: actualizar etiquetas y estructura en el conversor de moneda

---
+++ entregables/07-entregableConversor/js/conversorMain.js
@@ -0,0 +1,14 @@
+// Conversor de moneda JS
+
+//constantes
+const monedaSelect = document.getElementById('moneda');
+const cantidadInput = document.getElementById('cantidadUsuario');
+const btnConvertir = document.getElementById('btn_convertir');
+const resultadoYen = document.getElementById('monedaYen');
+const resultadoDolarNZ = document.getElementById('monedaNZ');
+const resultadoLibra = document.getElementById('monedaLE');
+
+//Funciones para convertir monedas
+function convertirMoneda(event){
+    
+};
