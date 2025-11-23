ea9c32559e10ed0363e514c5a9b233ceb131e9e4
ricardo milord <milord.dev05@gmail.com>
2025-11-23 16:22:47 -0500

    feat: mejorar la lógica de selección de moneda en el conversor

--- entregables/07-entregableConversor/js/conversorMain.js
+++ entregables/07-entregableConversor/js/conversorMain.js
@@ -7,13 +7,21 @@ const btnConvertir = document.getElementById('btn_convertir');
 const resultadoYen = document.getElementById('monedaYen');
 const resultadoDolarNZ = document.getElementById('monedaNZ');
 const resultadoLibra = document.getElementById('monedaLE');
+const error = document.getElementById('error');
 
 //Funciones para convertir monedas
 function seleccionMoneda(){
     if (monedaSelect.value === '1'){
-        
+        resultadoYen.value = cantidadInput.value * 154.12;
+        resultadoDolarNZ.value = cantidadInput.value * 1.78;
+        resultadoLibra.value = cantidadInput.value * 0.80;
     }
     else if (monedaSelect.value === '2'){
-        
+        resultadoYen.value = cantidadInput.value * 169.14;
+        resultadoDolarNZ.value = cantidadInput.value * 1.95;
+        resultadoLibra.value = cantidadInput.value * 0.88;
+    }
+    else{
+        error.innerText = 'Seleccione una moneda válida';
     }
 };
