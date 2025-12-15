e65748e2e7b93b2d42d2f4ce1d473c7fa9a407aa
ricardo milord <milord.dev05@gmail.com>
2025-12-15 01:50:45 -0500

    feat: añadir variables y comprobaciones iniciales en formPeliculas.js

--- entregables/08-pruebaPeliculas/js/formPeliculas.js
+++ entregables/08-pruebaPeliculas/js/formPeliculas.js
@@ -3,3 +3,12 @@
  */
 //variables 
 const nickInput = document.getElementById('nick');
+const emailInput = document.getElementById('email');
+const edadInput = document.getElementById('edad');
+const selectInputAnime = document.getElementById('generoAnime');
+const selectInputRomance = document.getElementById('generoRomance');
+const selectInputkDrama = document.getElementById('generoKDrama');
+const formulario = document.getElementById('formularioPelicula');
+const error = document.getElementById('error');
+
+//comprobacion de null
