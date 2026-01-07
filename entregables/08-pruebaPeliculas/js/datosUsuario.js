e2f31c80b32947c73991a1b87b17947f95f39a3a
ricardo milord <milord.dev05@gmail.com>
2026-01-06 22:34:09 -0500

    feat: agregar funcionalidad para mostrar datos del usuario y actualizar la gestión de datos en sessionStorage

--- entregables/08-pruebaPeliculas/js/datosUsuario.js
+++ entregables/08-pruebaPeliculas/js/datosUsuario.js
@@ -3,12 +3,10 @@
     Por ejemplo, guardar y obtener el nick del usuario en sessionStorage.
 */
 // El primer nick que se recibe en la funcion es un objeto input que guarda el nick del usuario
-function datosUsuario(nick) {
+function datosUsuario(nick, generoAnime, generoRomance, generoKDrama) {
     // este segundo nick es la clave valor para guardar en sessionStorage y recibe el valor del nick que recibimos arriba
     sessionStorage.setItem('nick', nick.value);
-}
-
-function obtenerNick(){
-    let nick = sessionStorage.getItem('nick');
-    console.log(nick);
+    sessionStorage.setItem('generoAnime', generoAnime.value);
+    sessionStorage.setItem('generoRomance', generoRomance.value);
+    sessionStorage.setItem('generoKDrama', generoKDrama.value);
 }
