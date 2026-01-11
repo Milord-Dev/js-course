c320313467ad95edfbac4853e01f8b32807e75af
ricardo milord <milord.dev05@gmail.com>
2026-01-11 14:02:57 -0500

    feat: corregir la gestión de la variable global 'nick' en datosUsuario.js

--- js/datosUsuario.js
+++ js/datosUsuario.js
@@ -1,10 +1,22 @@
 //Crear funciones para que guardar la sesión del usuario
 
+//variable global 
+let nick;
+
 function datosUsuario(nick){
     sessionStorage.setItem('nick', nick.value);
 }
 function obtenerNick(){
-    let nick = sessionStorage.getItem('nick');
+    nick = sessionStorage.getItem('nick');
     //monstrar por consola
     console.log(nick);
+}
+
+function comprobarUsuario(){
+    if (nick === null || nick === undefined || nick === '') {
+        return false;
+    }
+    else{
+        return true;
+    }
 }
