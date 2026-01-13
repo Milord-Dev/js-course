758d7dcec7eda0817c7295d8b99667da53b75573
ricardo milord <milord.dev05@gmail.com>
2026-01-13 02:04:33 -0500

    feat: agregar validación inicial y estructura al formulario de registro

---
+++ entregables/09-pruebaRegistro/js/registro.js
@@ -0,0 +1,20 @@
+//JS PARA LA COMPRIBACION DE REGISTRO
+
+//VARIBLES
+const nickInput = document.getElementById('nick');
+const correoInput = document.getElementById('correo');
+const contrasenaInput = document.getElementById('contrasena');
+const edadInput = document.getElementById('edad');
+const tamanoInput = document.getElementById('tamano');
+const formulario = document.getElementById('formularioEntrada')
+const error = document.getElementById('error')
+
+//COMPROBACION DE NULL EN LOS ELEMENTOS
+if(!nickInput || !correoInput || !contrasenaInput || !edadInput || !tamanoInput || !formulario || !error){
+    
+}
+
+//FUNCIONES DE COMPROBACIÓN
+function comprobarFormulario(event){
+
+}
