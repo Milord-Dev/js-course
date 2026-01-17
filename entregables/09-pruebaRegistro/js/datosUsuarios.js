5dd7823c559a3b7778ae04ec582ef4764ffda9e4
ricardo milord <milord.dev05@gmail.com>
2026-01-17 15:47:09 -0500

    feat: limpiar código y organizar la captura de datos del usuario en sessionStorage

--- entregables/09-pruebaRegistro/js/datosUsuarios.js
+++ entregables/09-pruebaRegistro/js/datosUsuarios.js
@@ -1,9 +1,5 @@
 // AQUI VAMOS A CAPTAR O GUARDAR LOS DATOS DEL USUARIO PARA EL SESSIONSTORAGE 
 
-//Varibles globales
-
-
-
 function datosUsuarios(nick,correo,edad,tamanoTexto){
     //llave valor
     sessionStorage.setItem('nick',nick.value);
@@ -12,11 +8,11 @@ function datosUsuarios(nick,correo,edad,tamanoTexto){
     sessionStorage.setItem('tamano', tamanoTexto);
 }
 
-function mostrarDatos(){
+/*function mostrarDatos(){
     const nickGuardado = sessionStorage.getItem('nick');
     const correoGuardado = sessionStorage.getItem('correo');
     const edadGuardada = sessionStorage.getItem('edad');
     const tamanoGuardado = sessionStorage.getItem('tamano');
     
     console.log(nickGuardado,correoGuardado,edadGuardada,tamanoGuardado);
-}
+}*/
--- entregables/09-pruebaRegistro/js/inicio.js
+++ entregables/09-pruebaRegistro/js/inicio.js
@@ -1,2 +1,7 @@
-//test
-mostrarDatos();
+//Donde captaremos lo datos de usuarios
+
+// variables globales 
+const nickGuardado = sessionStorage.getItem('nick');
+const correoGuardado = sessionStorage.getItem('correo');
+const edadGuardada = sessionStorage.getItem('edad');
+const tamanoGuardado = sessionStorage.getItem('tamano');
